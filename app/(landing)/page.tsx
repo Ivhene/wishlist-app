4eab261b5d34a80c562a1dbdf72046683650d040
Ivhene <ivarh.nesheim@gmail.com>
2025-01-08 14:51:15 +0100

    feat: basic setup

---
+++ app/(landing)/page.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <div className="">
+      <Button onClick={() => console.log("CLICKED")}>TESTING</Button>
+    </div>
+  );
+}
